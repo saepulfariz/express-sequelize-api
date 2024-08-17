2411dcde1e431da8d05a0aa61da309a01129d098
saepulfariz <saepulfariz108@gmail.com>
2024-08-17 13:53:41 +0700

    add delete data post

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.get('/posts', postController.getAllPosts);
 app.get('/posts/:id', postController.getPostById);
 app.post('/posts', postController.createPost);
 app.put('/posts/:id', postController.updatePost);
+app.delete('/posts/:id', postController.deletePost);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- controllers/postController.js
+++ controllers/postController.js
@@ -67,3 +67,21 @@ exports.updatePost = async (req, res) => {
         });
     }
 };
+
+exports.deletePost = async (req, res) => {
+    try {
+        const post = await Post.findByPk(req.params.id);
+        if (!post) return res.status(404).json({
+            message: 'Post not found'
+        });
+
+        await post.destroy();
+        res.json({
+            message: 'Post deleted successfully'
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        });
+    }
+};
