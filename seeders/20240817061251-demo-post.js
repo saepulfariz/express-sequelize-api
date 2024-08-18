9c5674e40ef3457614c89a75a2c378efe0a0dfd7
saepulfariz <saepulfariz108@gmail.com>
2024-08-18 14:16:04 +0700

    fix seed database

--- seeders/20240817061251-demo-post.js
+++ seeders/20240817061251-demo-post.js
@@ -5,11 +5,13 @@ module.exports = {
     return queryInterface.bulkInsert('Posts', [{
       title: 'First Post',
       content: 'Content of the first post',
+      userId: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       title: 'Second Post',
       content: 'Content of the second post',
+      userId: 2,
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
